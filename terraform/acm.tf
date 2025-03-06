65a7ccc5eb060cdb8e07222523ba2a05bc235da5
HamzahS-101 <hamzahsahal@gmail.com>
2025-03-06 17:23:21 +0000

    Added HTTPS listener to ALB

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,8 +1,33 @@
 resource "aws_acm_certificate" "cert" {
     domain_name = var.domain_name
     validation_method = "DNS"
+}
+resource "aws_route53_record" "cert-validation-record" {
+    for_each = {
+        for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+            name   = dvo.resource_record_name
+            record = dvo.resource_record_value
+            type   = dvo.resource_record_type
+            }
+        }
 
-    tags = {
-        Name = var.domain_name
-    }
+  zone_id = aws_route53_zone.my-hosted-zone.zone_id
+  name    = each.value.name
+  records = [each.value.record]
+  type    = each.value.type
+  ttl     = 60
+}
+
+resource "time_sleep" "wait_for_dns" {
+  depends_on = [aws_route53_record.cert-validation-record]
+
+  create_duration = "3m" 
+}
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert-validation-record : record.fqdn]
+  depends_on = [
+    time_sleep.wait_for_dns,
+    aws_route53_record.cert-validation-record
+  ]
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,9 +4,15 @@ terraform {
       source = "hashicorp/aws"
       version = "5.89.0"
     }
+    time = {
+        source = "hashicorp/time"
+        version = "~> 0.10.0"
+    }
   }
 }
 
 provider "aws" {
-  # Configuration options
+  region = "eu-west-2"
 }
+
+provider "time" {}
